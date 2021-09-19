80d02f4e888e1815287c744380b67bfdb6dffb42
plasch <>
2021-09-19 17:58:44 +0300

    add solutions 3B after review

---
+++ topic3/task_D_review.py
@@ -0,0 +1,15 @@
+# D.Угадай число
+
+n = int(input())
+possible = set(range(1, n + 1))
+s = input().strip()
+while s != "HELP":
+    nums = set(map(int, s.split()))
+    s = input().strip()
+    if s == "YES":
+        possible.intersection_update(nums)
+    else:
+        possible.difference_update(nums)
+    s = input().strip()
+
+print(*sorted(possible))
